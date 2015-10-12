d5b0e850b5a37fe1e3f1d05482306b977cc609ce
Edsel <edsel.lopez@jmaconsulting.biz>
2015-10-12 16:14:10 +0530

    MISC-76 Added changes to display miles from origin

--- views-view-fields--testgeo--page.tpl.php
+++ views-view-fields--testgeo--page.tpl.php
@@ -11,7 +11,7 @@ $params = array(
 $contact = civicrm_api3('Contact', 'get', $params);
 $phoneDiv = '';
 if ($contact['count'] != 0) {
-  $displayName = '';
+  $displayName = $disName = '';
   if ($contact['values'][$row->civicrm_address_contact_id]['api.Membership.get']['count'] != 0) {
     foreach ($contact['values'][$row->civicrm_address_contact_id]['api.Membership.get']['values'] as $key => $memberships) {
       if ($memberships['membership_type_id'] == 5) {
@@ -20,7 +20,7 @@ if ($contact['count'] != 0) {
       }
     }
   }
-  $contactLink = "/civicrm/contact/view?reset=1&cid={$row->civicrm_address_contact_id}";
+  $contactLink = "/mediator-profile/{$row->civicrm_address_contact_id}";
   $displayName .= "<a href='" . $contactLink . "'>" . $contact['values'][$row->civicrm_address_contact_id]['display_name'] . "</a>";
   if ($contact['values'][$row->civicrm_address_contact_id]['api.Phone.get']['count'] != 0) {
     foreach ($contact['values'][$row->civicrm_address_contact_id]['api.Phone.get']['values'] as $phones) {
@@ -28,26 +28,13 @@ if ($contact['count'] != 0) {
     }
   }
   $tempArray = $areas = array();
-  // Areas of interest
-  if (!empty($contact['values'][$row->civicrm_address_contact_id]['api.CustomValue.get']['values']) && !empty($contact['values'][$row->civicrm_address_contact_id]['api.CustomValue.get']['values'][2]['latest'])) {
-    $areas = array_filter($contact['values'][$row->civicrm_address_contact_id]['api.CustomValue.get']['values'][2]['latest']);
-  } 
   // Display Name
   if (!empty($contact['values'][$row->civicrm_address_contact_id]['api.CustomValue.get']['values']) && !empty($contact['values'][$row->civicrm_address_contact_id]['api.CustomValue.get']['values'][8]['latest'])) {
     $disName = $contact['values'][$row->civicrm_address_contact_id]['api.CustomValue.get']['values'][8]['latest'];
     $disName = "<a href='" . $contactLink . "'>" . $disName . "</a>";
   }
-  if (!empty($areas)) {
-    foreach ($areas as $area) {
-      $result = civicrm_api3('CustomField', 'getsingle', array(
-                  'sequential' => 1,
-                  'id' => AREAS,
-                ));
-      $options[AREAS] = array();
-      CRM_Core_BAO_CustomField::buildOption($result, $options[AREAS]);
-      $tempArray[] = $options[AREAS][$area];                             
-    }
-    $areas = implode(', ', $tempArray);
+  if ($disName == '') {
+    $disName = $displayName;
   }
   // Drupal Image
   $uid = CRM_Core_BAO_UFMatch::getUFId($row->civicrm_address_contact_id);
@@ -65,15 +52,15 @@ if ($contact['count'] != 0) {
 </div>
 <div class="views-field views-field-state">
   <span class="field-content"><?php echo $state; ?></span>
-</div>  
+</div>
+<div class="views-field views-field-distance">
+  <span class="field-content"><?php echo $row->field_data_field_geo_code_1_field_geofield_distance . ' miles'; ?></span>
+</div>
 <div class="views-field views-field-phone">
   <span class="field-content"><?php echo $phoneDiv; ?></span>
-</div>  
-<div class="views-field views-field-area">
-  <span class="field-content"><?php if (!empty($areas)) {echo $areas;} ?></span>
-</div>  
+</div>
 <div class="views-field views-field-picture">        
-<div class="field-content"><a href="/mediator-profile/198">  
+<div class="field-content"><a href="/mediator-profile/<?php echo $row->civicrm_address_contact_id; ?>">
 <div class="user-picture">
 <?php if (empty($user->picture)) {
  echo '<img typeof="foaf:Image" class="image-style-none" src="http://mcfm.hoster904.com/sites/default/files/user_pictures/filler_profile_pic.jpg" alt="yourfamilymatters\'s picture" title="yourfamilymatters\'s picture">';   
